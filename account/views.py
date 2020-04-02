63f9ac4076c78650cc7bc16729ec4397f586e909
Sang2306 <sangvorg@gmail.com>
2020-04-02 18:48:41 +0700

    add field to models.py|change template

--- account/views.py
+++ account/views.py
@@ -1,5 +1,10 @@
+import operator
+
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User, Permission
+from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpResponseGone
 from django.shortcuts import render, redirect
@@ -7,6 +12,7 @@ from django.urls import reverse
 from django.views.decorators.http import require_http_methods
 from django.core.mail import send_mail
 from django.conf import settings
+from django.views.generic import ListView
 
 
 class Login(LoginView):
@@ -42,11 +48,34 @@ def change_pwd(request):
         return redirect(to=reverse(viewname='blog:dashboard:list-article'))
 
 
+class ListUserView(LoginRequiredMixin, ListView):
+    model = User
+    ordering = ['username', 'first_name', 'last_name']
+    context_object_name = 'users'
+    template_name = 'account/user_list.html'
+
+
+def add_user(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    if User.objects.filter(username__exact=username).exists():
+        messages.add_message(request, messages.ERROR, 'User %s này đã tồn tại!' %username, 'danger')
+        return redirect(to='account:list-user')
+    user = User.objects.create_user(username=username, password=password)
+    # Set permissions for user who will be saved
+    add_articles = Permission.objects.get(codename__exact='add_articles')
+    change_articles = Permission.objects.get(codename__exact='change_articles')
+    user.user_permissions.add(add_articles, change_articles)
+    user.save()
+    return redirect(to='account:list-user')
+
+
 def reset_password(request):
-    #send_mail(
-    #    subject="Testing thoi",
-    #    message='hello ban sang',
-    #    from_email=settings.EMAIL_HOST_USER,
-    #    recipient_list=['sang.tanhle@gmail.com'],
-    #)
+    #todo chua xai dc
+    send_mail(
+        subject="Testing thoi",
+        message='hello ban sang',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=['sang.tanhle@gmail.com'],
+    )
     return HttpResponseGone()
