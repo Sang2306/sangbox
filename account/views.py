a3051e6cef9569424bc50adbf239c665de8bc28f
Sang2306 <30281263+Sang2306@users.noreply.github.com>
2020-05-14 18:17:03 +0700

    Update views.py

--- account/views.py
+++ account/views.py
@@ -72,10 +72,12 @@ def add_user(request):
 
 @api_view(['POST'])
 def check_login(request):
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    if username is None or password is None:
+    try:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None and user.is_active:
+            return JsonResponse(data={'success':True, 'user': user.id}, status=202)
+    except Exception as e:
         return JsonResponse(data={'success':False}, status=400)
-    user = authenticate(request, username=username, password=password)
-    if user is not None and user.is_active:
-        return JsonResponse(data={'success':True, 'user': user.id}, status=202)
+    return JsonResponse(data={'success':False}, status=404)
